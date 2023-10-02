a1205d99bd7e8e91173c0428bcb06ad36526414f
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-10-02 11:30:24 +0530

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className='w-screen h-screen bg-blue-100 flex justify-center items-center'>
+        <Link className='text-blue-900 text-4xl ' to="/page/1">See Our Cars</Link>
+    </div>
+  )
+}
+
+export default Home
